f68de0e0991e50b82ed65531e5101bbdd1d2e7ff
Xuehang Cang <xuehangcang@outlook.com>
2024-10-27 04:10:04 +0800

    feat:即将上线

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
+<!--      </nav>-->
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div class="bg-background h-screen flex items-center justify-center">
+
+        <div class="text-center">
+            <h1 class="text-white text-4xl md:text-6xl font-bold mb-4">OuleAI.org</h1>
+            <p class="text-gray-400 text-lg md:text-2xl">敬请期待，网站即将上线！</p>
+            <div class="mt-8">
+                <a href="mailto:ouleai@outlook.com" class="text-white border border-white px-6 py-3 rounded-full hover:bg-white hover:text-black transition">联系我们</a>
+            </div>
+        </div>
+
+    </div>
 </template>
